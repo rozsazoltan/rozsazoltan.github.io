095af5d25dbca642b40fef5f7dfceb9e690fd50d
Rózsa Zoltán <dev@datarose.net>
2023-07-30 16:37:20 +0200

    Portfolio v1.2.3.1

---
+++ portfolio/assets/js/en-a9666c78.js
@@ -0,0 +1,9 @@
+/**
+ * Datarose Production - @package portfolio
+ *
+ * @version v1.2.3.1
+ * @author Rózsa Zoltán
+ * @since 2022 - All rights reserved!
+ */
+
+import{f as e}from"./index-13b943a5.js";const n={"sotfware-developer":"Software Developer",backend:"backend",frontend:"frontend",design:"design / frontend",database:"database",integration:"integration",security:"security","version-control":"version control",extra:"extra","package-development":"package development","browser-extension":"Browser extension","read-more":"Read More",menu:{"as-developer":"as developer","as-partner":"as partner","privacy-policy":"privacy policy","not-found":"[the requested content cannot be found]"},header:{slogen:"make it work, make it right, make it fast"},home:{experience:"experience","experience-description":"Website, mobile application (Android and iOS), desktop application (Windows, macOS, Linux), browser extension development. Use of CentOS, knowledge of cPanel, domain management. Security and performance-oriented approach.",skills:"skills","skills-description":"I am proficient in both server-side and client-side work. I have a strong grasp of database structure creation, maintenance, and development. I am familiar with modern environments, version control systems, and frameworks.",interesting:"interesting facts","interesting-description":"..."},about:{"why-programming":"Why programming specifically?","why-programming-description":"I enjoy logic, developing new solutions, and playing chess. I willingly take on challenges and find satisfaction in creating things that can make others' work easier. I have a wealth of valuable experience that I want to leverage. However, I always feel that every day I gain a broader perspective on the world of software development, yet there is always more to learn. IT is evolving rapidly, and I need to keep up with it."}},t=e(n);export{t as default};
